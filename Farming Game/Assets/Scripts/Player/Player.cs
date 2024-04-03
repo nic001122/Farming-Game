aab6c8c2eaab4ccf2b6a6ff1190eda401eb97881
NewByList <tonatiuh@gmx.de>
2024-04-03 22:25:31 +0200

    visual changes to the player script

--- Farming Game/Assets/Scripts/Player/Player.cs
+++ Farming Game/Assets/Scripts/Player/Player.cs
@@ -23,27 +23,26 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-    mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-    Vector3Int cursorPosition = new Vector3Int((int)mousePos.x, (int)mousePos.y, 0);
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3Int cursorPosition = new Vector3Int((int)mousePos.x, (int)mousePos.y, 0);
 
-    if(GameManager.instance.tileManager.isInteractable(cursorPosition))
-    {
-        changeMouseClickPos();
-        defaultClickSprite = false;
-        Cursor.SetCursor(clickCursor, clickPos, CursorMode.Auto);
-        if(Input.GetKeyDown(KeyCode.F))
+        if(GameManager.instance.tileManager.isInteractable(cursorPosition))
         {
-            GameManager.instance.tileManager.SetInteracted(cursorPosition);
+            changeMouseClickPos();
+            defaultClickSprite = false;
+            Cursor.SetCursor(clickCursor, clickPos, CursorMode.Auto);
+            if(Input.GetKeyDown(KeyCode.F))
+            {
+                GameManager.instance.tileManager.SetInteracted(cursorPosition);
+            }
         }
-    }
 
-    else
-    {
-        changeMouseClickPos();
-        defaultClickSprite = true;
-        Cursor.SetCursor(defaultCursor, clickPos, CursorMode.Auto);
-    }
-
+        else
+        {
+            changeMouseClickPos();
+            defaultClickSprite = true;
+            Cursor.SetCursor(defaultCursor, clickPos, CursorMode.Auto);
+        }
     }
 
     public void DropItem(Item item)
