d5b473798d0c1d2a84f5c4f57660aa8a28a53ba7
NewByList <tonatiuh@gmx.de>
2024-04-05 23:24:15 +0200

    can now only plow if in a range of 2.5 units

--- Farming Game/Assets/Scripts/Managers/TileManager.cs
+++ Farming Game/Assets/Scripts/Managers/TileManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -9,6 +10,10 @@ public class TileManager : MonoBehaviour
 
     [SerializeField] private Tile hiddenInteractableTile;
     [SerializeField] private Tile interactedTile;
+
+    [SerializeField] GameObject player;
+    Vector2 playerPos;
+    Vector2 cursorPos;
 
     void Start()
     {
@@ -23,11 +28,22 @@ public class TileManager : MonoBehaviour
         }
     }
 
+    void Update()
+    {
+        playerPos = player.transform.position;
+        cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if(Input.GetKeyDown(KeyCode.T))
+        {
+            Debug.Log(Vector2.Distance(playerPos, cursorPos));
+            Debug.Log(cursorInRange());
+        }
+    }
+
     public bool isInteractable(Vector3Int position)
     {
         TileBase tile = interactableMap.GetTile(position);
 
-        if(tile != null)
+        if(tile != null && cursorInRange())
         {
             if(tile.name == "Interactable")
             {
@@ -41,5 +57,14 @@ public class TileManager : MonoBehaviour
     public void SetInteracted(Vector3Int position)
     {
         interactableMap.SetTile(position, interactedTile);
+    }
+
+    bool cursorInRange()
+    {
+        if(Vector2.Distance(playerPos, cursorPos) < 2.5 && Vector2.Distance(playerPos, cursorPos) > 0)
+        {
+            return true;
+        }
+        return false;
     }
 }
